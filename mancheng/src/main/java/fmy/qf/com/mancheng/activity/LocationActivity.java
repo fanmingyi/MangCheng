8f8c79e4a2e02a4dfa7a1d0ca4941f9cdaa336de
fanmingyi <fanmingyi>
2016-10-13 22:22:11 +0800

    完整百度地图定位 定位图片为滑稽

--- mancheng/src/main/java/fmy/qf/com/mancheng/activity/LocationActivity.java
+++ mancheng/src/main/java/fmy/qf/com/mancheng/activity/LocationActivity.java
@@ -1,100 +1,81 @@
 package fmy.qf.com.mancheng.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.location.Poi;
-
-import java.util.List;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 
 import fmy.qf.com.mancheng.R;
 
 public class LocationActivity extends AppCompatActivity {
     public LocationClient mLocationClient = null;
+    private MapView mapView;
+    private BaiduMap mBaiduMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_location);
         mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
-        initLocation();
-        mLocationClient.registerLocationListener(new BDLocationListener() {
-            @Override
-            public void onReceiveLocation(BDLocation location) {
-                //Receive Location
-                StringBuffer sb = new StringBuffer(256);
-                sb.append("time : ");
-                sb.append(location.getTime());
-                sb.append("\nerror code : ");
-                sb.append(location.getLocType());
-                sb.append("\nlatitude : ");
-                sb.append(location.getLatitude());
-                sb.append("\nlontitude : ");
-                sb.append(location.getLongitude());
-                sb.append("\nradius : ");
-                sb.append(location.getRadius());
-                if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果
-                    sb.append("\nspeed : ");
-                    sb.append(location.getSpeed());// 单位：公里每小时
-                    sb.append("\nsatellite : ");
-                    sb.append(location.getSatelliteNumber());
-                    sb.append("\nheight : ");
-                    sb.append(location.getAltitude());// 单位：米
-                    sb.append("\ndirection : ");
-                    sb.append(location.getDirection());// 单位度
-                    sb.append("\naddr : ");
-                    sb.append(location.getAddrStr());
-                    sb.append("\ndescribe : ");
-                    sb.append("gps定位成功");
 
-                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){// 网络定位结果
-                    sb.append("\naddr : ");
-                    sb.append(location.getAddrStr());
-                    //运营商信息
-                    sb.append("\noperationers : ");
-                    sb.append(location.getOperators());
-                    sb.append("\ndescribe : ");
-                    sb.append("网络定位成功");
-                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-                    sb.append("\ndescribe : ");
-                    sb.append("离线定位成功，离线定位结果也是有效的");
-                } else if (location.getLocType() == BDLocation.TypeServerError) {
-                    sb.append("\ndescribe : ");
-                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
-                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-                }
-                sb.append("\nlocationdescribe : ");
-                sb.append(location.getLocationDescribe());// 位置语义化信息
-                List<Poi> list = location.getPoiList();// POI数据
-                if (list != null) {
-                    sb.append("\npoilist size = : ");
-                    sb.append(list.size());
-                    for (Poi p : list) {
-                        sb.append("\npoi= : ");
-                        sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-                    }
-                }
-                Log.i("BaiduLocationApiDem", sb.toString());
-            }
-        });    //注册监听函数
+        mapView = ((MapView) findViewById(R.id.mapView));
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        mBaiduMap = mapView.getMap();
+        MapStatusUpdate  mapStatusUpdate2 = MapStatusUpdateFactory.zoomBy(7f);
 
+        mBaiduMap.setMapStatus(mapStatusUpdate2);
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initLocation();
+    }
+
+    BDLocationListener myListener =  new BDLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            if (null!= location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng latLng = new LatLng(latitude,longitude);
+                MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(mapStatusUpdate);
+                BitmapDescriptor bitmapDescriptor =null;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.huaji);
+                bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+                OverlayOptions options = new MarkerOptions().position(latLng).icon(bitmapDescriptor);
+                mBaiduMap.addOverlay(options);
+                mBaiduMap.hideInfoWindow();
+
+
+            }
+        }
+    };
     private void initLocation(){
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span=1000;
+        int span=0;
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true);//可选，默认false,设置是否使用gps
