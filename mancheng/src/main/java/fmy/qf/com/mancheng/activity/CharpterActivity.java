43cabd9beac9660180bf226d82b7a5f58dc9a1a0
fanmingyi <fanmingyi>
2016-10-14 10:02:54 +0800

    章节布局搭建

---
+++ mancheng/src/main/java/fmy/qf/com/mancheng/activity/CharpterActivity.java
@@ -0,0 +1,15 @@
+package fmy.qf.com.mancheng.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fmy.qf.com.mancheng.R;
+
+public class CharpterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charpter);
+    }
+}
